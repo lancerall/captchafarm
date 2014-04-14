d0e7897a24dc631c221b1cd7fba1929b3f01b8a9
Lance Rall <lancerall@charter.net>
2014-04-14 09:46:08 -0500

    Fixed cleanup for pollresponses.php for non-default file types

--- pollresponses.php
+++ pollresponses.php
@@ -4,25 +4,27 @@ require_once("config.php");
 $debug = false;
 $cleanup = false;
 $getchallenge = $_GET["challenge"];
+$textFile = $imagesDir.'/'.$getchallenge.".txt";
+$imageFile = $imagesDir.'/'.$getchallenge.$fileSuffix;
 
 if (ISSET($_GET["debug"])) $debug = true;
 if (ISSET($_GET["cleanup"])) $cleanup = true;
 
 if ($debug) print "<!-- Received request: $getchallenge -->\n";
-if ($debug) print "<!-- Looking for file $imagesDir/$getchallenge.txt -->\n";
+if ($debug) print "<!-- Looking for file $textFile -->\n";
 
-if (file_exists($imagesDir.'/'.$getchallenge.".txt")) {
+if (file_exists($textFile)) {
 	if ($debug) print "<!-- File exists. -->\n";
-	$text=file_get_contents($imagesDir."/".$getchallenge.".txt"); // grab the contents of the txt file if it exists
+	$text=file_get_contents($textFile); // grab the contents of the txt file if it exists
 	if ($debug) print "<!-- File contents are $text-->\n";
 	print $text;
 	
 	if ($text != ""){
-		if ($debug && $cleanup) print "<!-- Deleting $imagesDir/$getchallenge.txt -->\n";
-		if ($cleanup) unlink($imagesDir.'/'.$getchallenge.".txt"); #clean up
+		if ($debug && $cleanup) print "<!-- Deleting $textFile -->\n";
+		if ($cleanup) unlink($textFile); #clean up
 
-		if ($debug && $cleanup) print "<!-- Deleting $imagesDir/$getchallenge.png -->\n";
-		if ($cleanup) unlink($imagesDir.'/'.$getchallenge.".png"); #clean up
+		if ($debug && $cleanup) print "<!-- Deleting $imageFile -->\n";
+		if ($cleanup) unlink($imageFile); #clean up
 		
 	}
 }
