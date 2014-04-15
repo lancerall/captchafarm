7060bf8c6355d889d32b92d175769ca5273fa7e1
Lance Rall <lancerall@charter.net>
2014-04-15 17:13:45 -0500

    More concise code

--- captcha-form.php
+++ captcha-form.php
@@ -61,141 +61,120 @@ if (ISSET($_POST["response"])){
 }
 else if ($debug) print "<!-- Did not receive post. -->\n";
 
+$output = "";
+$statusboard = "";
+
+$statusboard .= '<div id="statusboard" style="width: 208px; border: 2px solid #ddd; padding:0px; float:left; margin:10px;">';
+
 $numFiles = 0;
 $thisdir = scandir($imagesDir);
-print '<dir id="statusboard" style="width: 208px; border: 2px solid #ddd; padding:0px; float:left; margin:10px;">';
 foreach($thisdir as $file){
-	if (substr($file,0,1) != "."){
-		$fullFileName = $imagesDir."/".$file;
-		$thisFileSuffix = substr($file,strlen($file)-strlen($fileSuffix),strlen($fileSuffix));
-		$thisFileNoSuffix = substr($file,0,strlen($file)-strlen($thisFileSuffix));
-		if ($thisFileSuffix == $fileSuffix) {
-			$numFiles++;
+	if (substr($file,0,1) != ".") {
+			$fullFileName = $imagesDir."/".$file;
+			$thisFileSuffix = substr($file,strlen($file)-strlen($fileSuffix),strlen($fileSuffix));
+			$thisFileNoSuffix = substr($file,0,strlen($file)-strlen($thisFileSuffix));
 			$textfile = substr($file,0,strlen($file)-strlen($fileSuffix)).".txt"; // construct .txt file name
 			$fullTextFileName = $imagesDir."/".$textfile;
-			if (file_exists($fullTextFileName)) {
-				$text=file_get_contents($fullTextFileName);
-				if ($text != "") {
-					$status="complete"; // This captcha has been solved
-				}
-				else {
-					$now = microtime(true);
-					$diff = $now - filemtime($fullTextFileName);
-					if ($diff > $lockFileExpirationSeconds) $status="open"; // Lock file exists but has expired
-					else $status="locked"; // Lock file exists and it is recent
-				}
-			}
-			else $status="open";
-			print '<div style="width:200px; border: 2px solid #444; margin: 2px;">';
-			if ($status == "complete") $icon = "yes.gif";
-			elseif ($status == "locked") $icon = "no.gif";
-			elseif ($status == "open") $icon = "blank.gif";
-			print '<img src="'.$icon.'" id="img'.$thisFileNoSuffix.'" width=30 style="vertical-align: middle;" />';
-			if ($status=="locked") {
-				$countdown = $diff - $lockFileExpirationSeconds;
-				
-				print '<script>
-				var count'.$thisFileNoSuffix.'='.abs(round($countdown)).';
-				var counter'.$thisFileNoSuffix.'=setInterval(timer'.$thisFileNoSuffix.', 1000);
-				function timer'.$thisFileNoSuffix.'()
-				{
-				  count'.$thisFileNoSuffix.'=count'.$thisFileNoSuffix.'-1;
-				  if (count'.$thisFileNoSuffix.' <= 0)
-				  {
-				     clearInterval(counter'.$thisFileNoSuffix.');
-					document.getElementById("'.$thisFileNoSuffix.'").innerHTML=""; 
-					document.getElementById("img'.$thisFileNoSuffix.'").src="blank.gif";
-				     return;
-				  }
+			$now = microtime(true);
 
-				 document.getElementById("'.$thisFileNoSuffix.'").innerHTML="("+count'.$thisFileNoSuffix.'+") "; 
+			if ($debug) print "<!-- Looking at $fullFileName ... -->\n";
+			if ($thisFileSuffix == $fileSuffix){ // if it's the right file type
+				if ($debug) print "<!-- $file is of type $fileSuffix ... -->\n";
+				$thisFileAge = $now - filemtime($fullFileName);
+				if ($thisFileAge > $captchaFileExpirationSeconds) {
+					if ($debug) print "<!-- This file is greater than $captchaFileExpirationSeconds seconds old. Deleting it. -->\n";
+					if (!$safe) unlink($fullFileName);
+					else if ($debug) print "<!-- SAFE MODE: Skipping delete of $fullFileName. -->\n";
+/*
+					if (file_exists($fullTextFileName)){
+						if (!$safe) unlink($fullTextFileName);
+						else if ($debug) print "<!-- SAFE MODE: Skipping delete of $fullTextFileName -->\n";
+					}
+*/
 				}
-				</script>';
-				print '<span id="'.$thisFileNoSuffix.'">('.abs(round($countdown)).') </span>';
-				}
-			print $file;
-			print '<img src="'.$fullFileName.'" style="width: 200px;" />';
-			print '</div>';
-		}
-	} 
-}
-if ($numFiles==0) print '<div style="width:200px; border: 2px solid #444; margin: 2px; text-align: center;">No Captcha Files.</div>';
-print '</dir>';
-
-
-$thisdir = scandir($imagesDir);
-foreach($thisdir as $file){
-	$fullFileName = $imagesDir."/".$file;
-	$thisFileSuffix = substr($file,strlen($file)-strlen($fileSuffix),strlen($fileSuffix));
-	$thisFileNoSuffix = substr($file,0,strlen($file)-strlen($thisFileSuffix));
-	$textfile = substr($file,0,strlen($file)-strlen($fileSuffix)).".txt"; // construct .txt file name
-	$fullTextFileName = $imagesDir."/".$textfile;
-	$now = microtime(true);
-	
-	if ($debug) print "<!-- Looking at $fullFileName ... -->\n";
-	if ($thisFileSuffix == $fileSuffix){ // if it's the right file type
-		if ($debug) print "<!-- $file is of type $fileSuffix ... -->\n";
-		$thisFileAge = $now - filemtime($fullFileName);
-		if ($thisFileAge > $captchaFileExpirationSeconds) {
-			if ($debug) print "<!-- This file is greater than $captchaFileExpirationSeconds seconds old. Deleting it. -->\n";
-			if (!$safe) unlink($fullFileName);
-			else if ($debug) print "<!-- SAFE MODE: Skipping delete of $fullFileName. -->\n";
-			if (file_exists($fullTextFileName)){
-				if (!$safe) unlink($fullTextFileName);
-				else if ($debug) print "<!-- SAFE MODE: Skipping delete of $fullTextFileName -->\n";
-			}
-		}
-		else{
-			if ($debug) print "<!-- This file is new enough to answer ($thisFileAge < $captchaFileExpirationSeconds seconds). Proceeding. -->\n";
-			if ($debug) print "<!-- Looking for text file named $fullTextFileName... -->\n";
-			if (file_exists($fullTextFileName)) {
-				if ($debug) print "<!-- The file exists. -->\n";
-				$text=file_get_contents($fullTextFileName); // grab the contents of the txt file if it exists
-				if ($text == "") {
-					if ($debug) print "<!-- Its contents are $text-->\n";
-					$diff = $now - filemtime($fullTextFileName);
-					if ($debug) print "<!-- Creation date is ".filemtime($fullTextFileName)." ... now is ".$now." ... diff is ".$diff." -->\n";
-					if ($diff > $lockFileExpirationSeconds) {
-						if ($debug) print "<!-- Lock file is over $lockFileExpirationSeconds seconds old, retrying... -->\n";
-						file_put_contents($fullTextFileName, ""); # Update modified date on lock file
-						$challengefile = $file; //winner
-						if ($debug) print "<!-- The challenge file is officially: $imagesDir/$challengefile -->\n";
-						break(1);
+				else{
+					if ($debug) print "<!-- This file is new enough to answer ($thisFileAge < $captchaFileExpirationSeconds seconds). Proceeding. -->\n";
+					if ($debug) print "<!-- Looking for text file named $fullTextFileName... -->\n";
+					if (file_exists($fullTextFileName)) {
+						if ($debug) print "<!-- The file exists. -->\n";
+						$text=file_get_contents($fullTextFileName); // grab the contents of the txt file if it exists
+						if ($text == "") {
+							if ($debug) print "<!-- Its contents are $text-->\n";
+							$diff = $now - filemtime($fullTextFileName);
+							if ($debug) print "<!-- Creation date is ".filemtime($fullTextFileName)." ... now is ".$now." ... diff is ".$diff." -->\n";
+							if ($diff > $lockFileExpirationSeconds) {
+								if ($debug) print "<!-- Lock file is over $lockFileExpirationSeconds seconds old, retrying... -->\n";
+								$status="open"; // Lock file exists but has expired
+								$numFiles++;
+								$statusboard .= create_statusboard_block($file, $fullFileName, $thisFileNoSuffix, $status, $diff);
+								if ($challengefile == "") {
+									file_put_contents($fullTextFileName, ""); # Update modified date on lock file
+									$challengefile = $file; //winner
+									$challengetextfile = $textfile;
+									if ($debug) print "<!-- The challenge file is officially: $imagesDir/$challengefile -->\n";
+								}
+								//break(1);
+							}
+							else {
+								$status="locked"; // Lock file exists and it is recent
+								$numFiles++;
+								$statusboard .= create_statusboard_block($file, $fullFileName, $thisFileNoSuffix, $status, $diff);
+							}
+						}
+						else {
+							if ($debug) print "<!-- Skipping file since this captcha has already been answered: $text -->\n";
+							$status="complete";
+							$numFiles++;
+							$statusboard .= create_statusboard_block($file, $fullFileName, $thisFileNoSuffix, $status, $diff);
+						}
+					}
+					else { // no text file exists, so this one is valid to use
+						$status="open";
+						$numFiles++;
+						$statusboard .= create_statusboard_block($file, $fullFileName, $thisFileNoSuffix, $status, $diff);
+						if ($debug) print "<!-- The file does not exist, so I am creating one: $fullTextFileName -->\n";
+						if ($challengefile == "") {
+							file_put_contents($fullTextFileName, "");
+							$challengefile = $file; //winner
+							$challengetextfile = $textfile;
+							if ($debug) print "<!-- The challenge file is officially: $imagesDir/$challengefile -->\n";
+						}
+						//break(1);
 					}
 				}
-				else if ($debug) print "<!-- Skipping file since this captcha has already been answered: $text -->\n";
 			}
-			else { // no text file exists, so this one is valid to use
-				if ($debug) print "<!-- The file does not exist, so I am creating one: $fullTextFileName -->\n";
-				file_put_contents($fullTextFileName, "");
-				$challengefile = $file; //winner
-				if ($debug) print "<!-- The challenge file is officially: $imagesDir/$challengefile -->\n";
-				break(1);
-			}
-		}
-	}
-	else{
-		if ($debug) print "<!-- File is not of type $fileSuffix -->\n";
-		if (substr($file,0,1) != "."){ # Please do not delete . or .. or .files
-			if ($thisFileSuffix == ".txt") {# Please do not delete lock files that do not have corresponding captchas
-				if (file_exists($imagesDir."/".$thisFileNoSuffix.$fileSuffix)) {
-					# Do nothing.
-					if ($debug) print "<!-- Skipping cleanup of this txt file since $imagesDir/$thisFileNoSuffix$fileSuffix exists. -->\n";
-				}
-				else if ($cleanup) {
-					if ($debug) print "<!-- Purging $fullFileName -->\n";
-					if (!$safe) unlink($fullFileName);
-					else if ($debug) print "<!-- SAFE MODE: Skipping delete. -->\n";
+			else{
+				if ($debug) print "<!-- File is not of type $fileSuffix -->\n";
+				if (substr($file,0,1) != "."){ # Please do not delete . or .. or .files
+					if ($thisFileSuffix == ".txt") {# Please do not delete lock files that do not have corresponding captchas
+						if (file_exists($imagesDir."/".$thisFileNoSuffix.$fileSuffix)) {
+							# Do nothing.
+							if ($debug) print "<!-- Skipping cleanup of this txt file since $imagesDir/$thisFileNoSuffix$fileSuffix exists. -->\n";
+						}
+						else if ($cleanup) {
+							if ($debug) print "<!-- Purging $fullFileName -->\n";
+							if (!$safe) unlink($fullFileName);
+							else if ($debug) print "<!-- SAFE MODE: Skipping delete. -->\n";
+						}
+					}
+					else if ($cleanup) {
+						if ($debug) print "<!-- Purging $fullFileName -->\n";
+						if (!$safe) unlink($fullFileName);
+						else if ($debug) print "<!-- SAFE MODE: Skipping delete. -->\n";
+					}
 				}
 			}
-			else if ($cleanup) {
-				if ($debug) print "<!-- Purging $fullFileName -->\n";
-				if (!$safe) unlink($fullFileName);
-				else if ($debug) print "<!-- SAFE MODE: Skipping delete. -->\n";
-			}
+
+			
 		}
 	}
-}
+
+
+
+if ($numFiles==0) $statusboard .= '<div style="width:200px; border: 2px solid #444; margin: 2px; text-align: center; padding-top: 10px; padding-bottom: 10px;">No Captcha files.</div>';
+
+$statusboard .= "</div>";
+print $statusboard;
 
 if (ISSET($challengefile)) {
 	print '<div id="captchaformdiv" style="border: 2px solid #ddd; width: 500px; float: left; margin-top: 10px; padding: 10px;">';
@@ -211,7 +190,7 @@ if (ISSET($challengefile)) {
 		
 	print "\t".'<input type="text" name="response" />'."\n";
 	
-	print "\t".'<input type="hidden" name="textfile" value="'.$textfile.'" />'."\n";
+	print "\t".'<input type="hidden" name="textfile" value="'.$challengetextfile.'" />'."\n";
 	
 	print "\t".'<input type="submit" value="Submit">'."\n";
 	
@@ -227,3 +206,46 @@ else {
 ?>
 </body>
 </html>
+
+<?php
+
+
+function create_statusboard_block($file, $fullFileName, $thisFileNoSuffix, $status, $diff){
+	GLOBAL $lockFileExpirationSeconds;
+	
+	$statusboard .= "\n\t".'<div style="width:200px; border: 2px solid #444; margin: 2px;">'."\n";
+	if ($status == "complete") $icon = "yes.gif";
+	elseif ($status == "locked") $icon = "no.gif";
+	elseif ($status == "open") $icon = "blank.gif";
+	$statusboard .= "\t\t".'<img src="'.$icon.'" id="img'.$thisFileNoSuffix.'" width=30 style="vertical-align: middle;" />'."\n";
+	if ($status=="locked") {
+		$countdown = $diff - $lockFileExpirationSeconds;
+
+		$statusboard .= "\t\t".'<script>
+		var count'.$thisFileNoSuffix.'='.abs(round($countdown)).';
+		var counter'.$thisFileNoSuffix.'=setInterval(timer'.$thisFileNoSuffix.', 1000);
+		function timer'.$thisFileNoSuffix.'()
+		{
+		  count'.$thisFileNoSuffix.'=count'.$thisFileNoSuffix.'-1;
+		  if (count'.$thisFileNoSuffix.' <= 0)
+		  {
+		     clearInterval(counter'.$thisFileNoSuffix.');
+			document.getElementById("'.$thisFileNoSuffix.'").innerHTML=""; 
+			document.getElementById("img'.$thisFileNoSuffix.'").src="blank.gif";
+		     return;
+		  }
+
+		 document.getElementById("'.$thisFileNoSuffix.'").innerHTML="("+count'.$thisFileNoSuffix.'+") "; 
+		}
+		</script>'."\n";
+		$statusboard .= "\t\t".'<span id="'.$thisFileNoSuffix.'">('.abs(round($countdown)).') </span>';
+		}
+	$statusboard .= $file;
+	$statusboard .= "\t".'<img src="'.$fullFileName.'" style="width: 200px;" />'."\n";
+	$statusboard .= "\t".'</div>';
+	
+	return $statusboard;
+}
+
+
+?>
