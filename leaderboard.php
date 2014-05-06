c5b6e61b1071e9e2b34b7fc6e0f4201de0d3c236
Lance Rall <lancerall@charter.net>
2014-05-06 13:40:40 -0500

    Adding progress bar style to leaderboard

--- leaderboard.php
+++ leaderboard.php
@@ -2,7 +2,7 @@
 
 include('config/config.php');
 
-print '<div id="leaderboard" style="border: 2px solid #444; width: 200px; float: right; margin: 10px; padding: 2px;">'."\n";
+print '<div id="leaderboard" style="border: 2px solid #444; width: 200px; float: right; margin: 10px; padding: 2px;background-image:url(\'img/grad-c-f.jpg\'); background-size:100% 100%">'."\n";
 print '<div style="text-align: center;">Leaderboard</div>';
 $leaders = array();
 $thisdir = scandir($leaderboardDir);
@@ -14,10 +14,18 @@ foreach($thisdir as $file){
 	}
 }
 
+$maxwidth=208;
+$maxscore=0;
+$thermometerImage = 'img/grad-ccf.gif';
+$thermometerBackground = '#ddf';
+
 arsort($leaders);
 
 foreach($leaders as $name => $score){
-	print '<div style="border: 2px solid #ddd; margin: 2px; padding:2 10 2 2; text-align: right">';
+	if (intval($score) > $maxscore) {$maxscore = $score; }
+	$relativesize = round(($score/$maxscore) * $maxwidth);
+	print '<div style="border: 2px solid #ddd; margin: 2px; padding:2 10 2 2; text-align: right; background-color: '.$thermometerBackground.'; background-image:url(\''.$thermometerImage.'\'); background-size:'.$relativesize.'px 100%; background-repeat:repeat-y; background-position: left;">';
+	
 	print $name." : ".$score;
 	print "</div>\n";
 	$total += $score;
